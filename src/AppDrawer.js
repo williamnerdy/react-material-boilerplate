7a169091ee245dc8d1da334441e01c836012df0f
William Cunha <williamoliveira2@hotmail.com>
2018-08-18 12:10:41 -0300

    Add toobar menus

--- src/AppDrawer.js
+++ src/AppDrawer.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
 import Hidden from '@material-ui/core/Hidden';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import PeopleIcon from '@material-ui/icons/People';
 import Root from './screens/Root';
 
 const drawerWidth = 240;
@@ -23,6 +31,15 @@ const styles = theme => ({
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1
+  },
+  appToolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  appToolbarGroup: {
+    display: 'flex',
+    alignItems: 'center'
   },
   drawerPaper: {
     position: 'relative',
@@ -42,33 +59,94 @@ const styles = theme => ({
   toolbar: theme.mixins.toolbar
 });
 
-class AppDrawer extends React.Component {
+type Props = {
+  classes: object,
+  theme: object,
+  children: React.Node,
+  title: string
+};
+
+class AppDrawer extends Component<Props> {
   state = {
-    mobileOpen: false
+    mobileOpen: false,
+    anchorEl: null
   };
 
   handleDrawerToggle = () => {
     this.setState(state => ({ mobileOpen: !state.mobileOpen }));
   };
 
+  handleOpenMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleCloseMenu = () => {
+    this.setState({ anchorEl: null });
+  };
+
   render() {
     const { classes, theme } = this.props;
+    const { anchorEl } = this.state;
 
     return (
       <div className={classes.root}>
         <AppBar className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerToggle}
-              className={classes.navIconHide}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" noWrap>
-              {this.props.title}
-            </Typography>
+          <Toolbar className={classes.appToolbar}>
+            <div className={classes.appToolbarGroup}>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classes.navIconHide}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" noWrap>
+                {this.props.title}
+              </Typography>
+            </div>
+            <div className={classes.appToolbarGroup}>
+              <IconButton
+                color="inherit"
+                aria-label="More"
+              >
+                <NotificationsIcon />
+              </IconButton>
+              <IconButton
+                color="inherit"
+                aria-label="More"
+                aria-owns={anchorEl ? 'long-menu' : null}
+                aria-haspopup="true"
+                onClick={this.handleOpenMenu}
+              >
+                <MoreVertIcon />
+              </IconButton>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={this.handleCloseMenu}
+              >
+                <MenuItem onClick={this.handleClose}>
+                  <ListItemIcon>
+                    <AccountCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText inset primary="Minha Conta" />
+                </MenuItem>
+                <MenuItem onClick={this.handleClose}>
+                  <ListItemIcon>
+                    <PeopleIcon />
+                  </ListItemIcon>
+                  <ListItemText inset primary="Administradores" />
+                </MenuItem>
+                <MenuItem onClick={this.handleClose}>
+                  <ListItemIcon>
+                    <ExitToAppIcon />
+                  </ListItemIcon>
+                  <ListItemText inset primary="Sair" />
+                </MenuItem>
+              </Menu>
+            </div>
           </Toolbar>
         </AppBar>
         <Hidden mdUp>
@@ -106,12 +184,5 @@ class AppDrawer extends React.Component {
     );
   }
 }
-
-AppDrawer.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
-  children: PropTypes.object,
-  title: PropTypes.string
-};
 
 export default withStyles(styles, { withTheme: true })(AppDrawer);
